68335478e794878c82a1c18a7ff0398e4b05424b
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-10 11:49:14 -0800

    complete

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
+});
+
+app.post("/api/fileanalyse", multer().single("upfile"), (request, response) => {
+  let responseObject = {};
+  responseObject.name = request.file.originalname;
+  responseObject.type = request.file.mimetype;
+  responseObject.size = request.file.size;
+
+  response.json(responseObject);
 });
